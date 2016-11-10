2de696a6b36372e4fe3e96b5f6008f7b6f5fb68b
Andy Walpole <andywalpole@Andys-iMac.local>
2016-11-10 15:18:37 +0000

    added moongose model UserClass

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const router = require('./router');
+const ModelClass = require('./models/user');
 
 // App Set up
 app.use(morgan('combined'));
 app.use(bodyParser.json({type: '*/*'}));
-
+router(app);
 
 // Server setup
 const port = process.env.port || 3090;
---
+++ router.js
@@ -0,0 +1,13 @@
+module.exports = function(app) {
+
+	app.get('/', (req, res, next) => {
+
+		res.send([
+			`waterbottle`,
+			`phone`,
+			`paper`
+		]);
+
+	});
+
+};
