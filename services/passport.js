8912c30bf3578f7e4b95c19251aeefbaa72f982f
Andy Walpole <me@andywalpole.me>
2016-11-11 12:23:50 +0000

    added passport-local npm and code

--- services/passport.js
+++ services/passport.js
@@ -3,3 +3,53 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+// Create local strategy
+const localLogin = new LocalStrategy({
+	usernameField: 'email'
+}, (email, password, done) => {
+	// Verify this username and password, call done with the user
+	// if it is the correct username and password
+	// otherwise call done with false
+
+	User.findOne({email: email}, (err, user) => {
+
+		if (err) {
+			return done(err);
+		}
+
+		!user ? done(null, false) : done();
+
+		// compare passwords - is 'password' equal to user.password
+
+	});
+
+});
+
+// Setup options for JWT strategy
+const jwtOptions = {
+	jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+	secretOrKey: config.secret
+};
+
+// Create JWT strategy
+const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
+	// See if the user ID in the payload exists in our database
+	// if it does, call done with that other
+	// otherwise, call done without a user object
+
+	User.findById(payload.sub, (err, user) => {
+
+		if (err) {
+			return done(err, false);
+		}
+
+		user ? done(null, user) : done(null, false);
+
+	});
+
+});
+
+// Tell passport to use this strategy
+passport.use(jwtLogin);
